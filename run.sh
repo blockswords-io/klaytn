0873b89fc2ca752c9bc856ae2655bf6a0925ec30
ollie.j <ollie.j@krustuniverse.com>
2022-05-13 14:49:07 +0900

    Add run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #1/bin/sh
+set -ex
+
+docker build -t klaytn/klaytn:contractgov .
+
 cd build/bin
 
-./homi setup --cn-num 1 --governance --ist-epoch 30
+./homi setup --cn-num 1 \
+    --governance \
+    --ist-epoch 30 \
+    --docker-image-id klaytn/klaytn:contractgov
+
 cd homi-output
 docker-compose up
